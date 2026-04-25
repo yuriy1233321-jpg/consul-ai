92f32703770da85c61bbb4b28f89cdb7a0447edd
yuriy1233321-jpg <yuriy1233321@gmail.com>
2026-04-25 13:08:17 +0200

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,1 +1,39 @@
 
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: '/',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  res => res,
+  err => {
+    if (err.response?.status === 401) {
+      localStorage.removeItem('token');
+      window.location.reload();
+    }
+    return Promise.reject(err);
+  }
+);
+
+export const register = (email, password, name) =>
+  api.post('/api/register', { email, password, name });
+
+export const login = (email, password) =>
+  api.post('/api/login', { email, password });
+
+export const sendMessage = (message) =>
+  api.post('/chat', { message });
+
+export default api;
