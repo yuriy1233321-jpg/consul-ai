2c28899795db713dd7673497d211ce2a20e4b723
yuriy1233321-jpg <yuriy1233321@gmail.com>
2026-04-25 14:29:27 +0200

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,13 @@
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist"
+  server: {
+    proxy: {
+      '/chat': 'http://localhost:3000',
+      '/api': 'http://localhost:3000'
+    }
   }
 });
