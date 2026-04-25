d3e300969018c26bf65f2834e8a91f36b9a4b8b6
yuriy1233321-jpg <yuriy1233321@gmail.com>
2026-04-25 12:38:45 +0200

    Create vite.config.js

---
+++ client/src/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/chat': 'http://localhost:3000',
+      '/api': 'http://localhost:3000'
+    }
+  }
+})
