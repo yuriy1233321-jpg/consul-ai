06998355c39151b3c1c98c2e36fb0c08860fc52a
Yurii <Yuriy1233321@gmail.com>
2026-05-25 00:24:12 +0200

    fix vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    allowedHosts: ['.up.railway.app'],
+  },
 })
