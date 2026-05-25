fb18d9c49f04ac870a027507e4335de8efef37da
Yurii <Yuriy1233321@gmail.com>
2026-05-25 02:05:24 +0200

    add firebase auth

---
+++ server/services/auth.js
@@ -0,0 +1,60 @@
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut
+} from "firebase/auth";
+
+import { auth }
+from "../firebase/firebase";
+
+const provider =
+new GoogleAuthProvider();
+
+
+export async function loginGoogle(){
+
+return await signInWithPopup(
+auth,
+provider
+);
+
+}
+
+
+export async function registerEmail(
+email,
+password
+){
+
+return await createUserWithEmailAndPassword(
+auth,
+email,
+password
+);
+
+}
+
+
+export async function loginEmail(
+email,
+password
+){
+
+return await signInWithEmailAndPassword(
+auth,
+email,
+password
+);
+
+}
+
+
+export async function logout(){
+
+return await signOut(
+auth
+);
+
+}
